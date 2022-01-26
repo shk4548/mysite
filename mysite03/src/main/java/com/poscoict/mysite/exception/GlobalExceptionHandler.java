88721861400c5577feea2ae7a71c6ebafa0d6344
BIT <BIT@DESKTOP-8FPA74D>
2022-01-26 12:39:59 +0900

    logging 적용

--- mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscoict/mysite/exception/GlobalExceptionHandler.java
@@ -3,21 +3,24 @@ package com.poscoict.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice // advice 가 붙으면 aop 라고 생각하면 됨
 public class GlobalExceptionHandler {
+	//로그
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
 	
 	@ExceptionHandler(Exception.class)
 	public String ExceptionHandler(Model model, Exception e) {
 		// 1. 로깅
-		// String errors = e.toString();
 		
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString());
+		LOGGER.error(errors.toString());
 		
 		
 		// 2. 사과 페이지 ( HTML응답, 정상 종료 )
