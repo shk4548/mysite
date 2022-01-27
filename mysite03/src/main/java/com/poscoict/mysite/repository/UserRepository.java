f0c5eed58da11368bf311823c3e1636630a3df0a
BIT <BIT@DESKTOP-8FPA74D>
2022-01-27 12:54:09 +0900

    mysite

--- mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/UserRepository.java
@@ -3,11 +3,10 @@ package com.poscoict.mysite.repository;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.sql.DataSource;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.poscoict.mysite.exception.UserRepositoryException;
 import com.poscoict.mysite.vo.UserVo;
@@ -23,8 +22,7 @@ public class UserRepository {
 	}
 	
 	public boolean update(UserVo vo) {
-		int count = 0;
-		sqlSession.update("user.update",vo);
+		int count = sqlSession.update("user.update",vo);;
 		return count == 1;
 
 	}
@@ -34,11 +32,15 @@ public class UserRepository {
 	}
 	
 	public UserVo findByEmailAndPassword(String email, String password) throws UserRepositoryException{
+		;
+		
 		Map<String, String> map = new HashMap<>();
 		map.put("e", email);
 		map.put("p", password);
 		
-		return sqlSession.selectOne("user.findByEmailAndPassword", map);
+		UserVo vo = sqlSession.selectOne("user.findByEmailAndPassword", map);
+
+		return vo;
 	}
 
 
